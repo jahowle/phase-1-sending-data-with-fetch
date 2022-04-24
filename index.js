70f9478eef0e22545307cd876646d26e7e45b81a
Jonny Howle <jonny.howle@gmail.com>
2022-04-24 13:59:44 -0600

    commit tree 22348cae39432dbe59223603059861a7011855a4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData (userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        const h1 = document.createElement("h1");
+        h1.innerHTML = object.id;
+        document.body.appendChild(h1);
+      })
+      .catch(function (error) {
+          const h2 = document.createElement("h2");
+          h2.innerHTML = error.message;
+          document.body.appendChild(h2);
+      });
+      
+};
+
